06386bc6144bdc0b4ec29b17712aad730d519a40
lazer-ecomm-dev <ecomm@lazertechnologies.com>
2026-01-20 21:19:01 +0600

    AUTH MIDDLEWARE

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,2 +1,13 @@
 // Auth Middleware
 // middleware/auth.middleware.js
+
+const jwt = require('jsonwebtoken');
+module.exports = (req, res, next) => {
+  console.log('Auth Middleware');
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ msg: 'No Token' });
+  }
+  req.user = jwt.verify(token, process.env.JWT_SECRET);
+  next();
+};
